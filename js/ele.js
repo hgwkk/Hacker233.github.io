3f9580fec51215ec785cee9ed00829474d0deb1e
newred <lhq223399@outlook.com>
2017-08-06 08:41:43 +0800

    1

--- js/ele.js
+++ js/ele.js
@@ -1,9 +1,3 @@
 window.onload=function() {
-    var oIn=document.getElementsByClassName('catalog_in');
-    for(var i=0;i<oIn.length;i++)
-    {
-        oIn[i].onmouseover=function() {
-            oIn[i].style.backgroundColor="red";
-        }
-    }
+
 }
